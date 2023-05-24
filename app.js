236bafbd340324e9bdbcc5cafb430d737e9980ed
DAVID LOPES <davidlopes0208@gmail.com>
2023-05-24 08:28:36 +0200

    contact us and top3 pretty much done need some fixes on the modals in page top3

--- app.js
+++ app.js
@@ -1,85 +1,85 @@
 ///////////////////// NAVBAR JS ///////////////////////////////////////
 
-const sidenav = document.getElementById("mySidenav");
-const openBtn = document.getElementById("openBtn");
-openBtn.onclick = closeOrOpenNav;
+const sidenav = document.getElementById("mySidenav")
+const openBtn = document.getElementById("openBtn")
+openBtn.onclick = closeOrOpenNav
 function closeOrOpenNav() {
   if (sidenav.classList.contains("active")) {
-    sidenav.classList.remove("active");
-    sidenav.classList.add("transitionOut");
+    sidenav.classList.remove("active")
+    sidenav.classList.add("transitionOut")
   } else {
-    sidenav.classList.add("active");
-    sidenav.classList.remove("transitionOut");
+    sidenav.classList.add("active")
+    sidenav.classList.remove("transitionOut")
   }
 }
 ///////////////////// NAVBAR JS ///////////////////////////////////////
 
 ///////////////////// DROPDONW AND FILTER JS ///////////////////////////////////////
-filterSelection("all");
+filterSelection("all")
 function filterSelection(c) {
-  let x, i;
-  x = document.querySelectorAll(".filterDiv");
-  if (c == "all") c == "";
+  let x, i
+  x = document.querySelectorAll(".filterDiv")
+  if (c == "all") c == ""
   for (i = 0; i < x.length; i++) {
-    removeClass(x[i], "showFilteredElements");
-    if (x[i].className.indexOf(c) > -1) addClass(x[i], "showFilteredElements");
+    removeClass(x[i], "showFilteredElements")
+    if (x[i].className.indexOf(c) > -1) addClass(x[i], "showFilteredElements")
   }
 }
 // montrer les elements selectioner
 function addClass(element, name) {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
+  let i, arr1, arr2
+  arr1 = element.className.split(" ")
+  arr2 = name.split(" ")
   for (i = 0; i < arr2.length; i++)
     if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+      element.className += " " + arr2[i]
     }
 }
 // cacher les elemens non selectione
 function removeClass(element, name) {
-  let i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
+  let i, arr1, arr2
+  arr1 = element.className.split(" ")
+  arr2 = name.split(" ")
   for (i = 0; i < arr2.length; i++) {
     while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
+      arr1.splice(arr1.indexOf(arr2[i]), 1)
     }
   }
-  element.className = arr1.join(" ");
+  element.className = arr1.join(" ")
 }
 //ajout de la classe "active" sur le bouton actuel choisi
 
-let dropDownContainer = document.querySelector("#myDropdown");
+let dropDownContainer = document.querySelector("#myDropdown")
 
-let choiceSelection = dropDownContainer.querySelectorAll("a");
+let choiceSelection = dropDownContainer.querySelectorAll("a")
 
 choiceSelection.forEach(function (choice) {
   choice.addEventListener("click", function () {
-    myDropDownMenu();
-  });
-});
+    myDropDownMenu()
+  })
+})
 
-let current = document.querySelectorAll(".active");
+let current = document.querySelectorAll(".active")
 current.forEach(function (element) {
-  element.className = element.className.replace("active", "");
-});
+  element.className = element.className.replace("active", "")
+})
 
 function myDropDownMenu() {
-  document.getElementById("myDropdown").classList.toggle("show");
+  document.getElementById("myDropdown").classList.toggle("show")
 }
 
 function filterFunction() {
-  let input, filter, ul, li, a, i;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  div = document.getElementById("myDropdown");
-  a = div.getElementsByTagName("a");
+  let input, filter, ul, li, a, i
+  input = document.getElementById("myInput")
+  filter = input.value.toUpperCase()
+  div = document.getElementById("myDropdown")
+  a = div.getElementsByTagName("a")
   for (i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
+    txtValue = a[i].textContent || a[i].innerText
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
-      a[i].style.display = "";
+      a[i].style.display = ""
     } else {
-      a[i].style.display = "none";
+      a[i].style.display = "none"
     }
   }
 }
@@ -89,64 +89,64 @@ function filterFunction() {
 ///////////////////// Click pop up ////////////////////////////////////////////////
 
 //when the user click on Auguste picture
-const clickAuguste = document.querySelector("#popupAuguste");
+const clickAuguste = document.querySelector("#popupAuguste")
 
-const popupAuguste = document.querySelector("#popupBakeryAuguste");
+const popupAuguste = document.querySelector("#popupBakeryAuguste")
 
-const main = document.querySelector(".main");
+const main = document.querySelector(".main")
 
-const closeButAuguste = document.querySelector("#close-btnAuguste");
+const closeButAuguste = document.querySelector("#close-btnAuguste")
 
-const body = document.querySelector("body");
+const body = document.querySelector("body")
 
 clickAuguste.addEventListener("click", function () {
-  popupAuguste.style.display = "flex";
-  main.classList.add("activePopup");
-  body.classList.add("bodyActivePopup");
-});
+  popupAuguste.style.display = "flex"
+  main.classList.add("activePopup")
+  body.classList.add("bodyActivePopup")
+})
 
 closeButAuguste.addEventListener("click", function () {
-  popupAuguste.style.display = "none";
-  main.classList.remove("activePopup");
-  body.classList.remove("bodyActivePopup");
-});
+  popupAuguste.style.display = "none"
+  main.classList.remove("activePopup")
+  body.classList.remove("bodyActivePopup")
+})
 
 //when the user click on Toque picture
-const clickToque = document.querySelector("#popupToque");
+const clickToque = document.querySelector("#popupToque")
 
-const popupToque = document.querySelector("#popupBakeryToque");
+const popupToque = document.querySelector("#popupBakeryToque")
 
-const closeButToque = document.querySelector("#close-btnToque");
+const closeButToque = document.querySelector("#close-btnToque")
 
 clickToque.addEventListener("click", function () {
-  popupToque.style.display = "flex";
-  main.classList.add("activePopup");
-  body.classList.add("bodyActivePopup");
-});
+  popupToque.style.display = "flex"
+  main.classList.add("activePopup")
+  body.classList.add("bodyActivePopup")
+})
 
 closeButToque.addEventListener("click", function () {
-  popupToque.style.display = "none";
-  main.classList.remove("activePopup");
-  body.classList.remove("bodyActivePopup");
-});
+  popupToque.style.display = "none"
+  main.classList.remove("activePopup")
+  body.classList.remove("bodyActivePopup")
+})
 
 //when the user click on Eric picture
-const clickEric = document.querySelector("#popupEric");
+const clickEric = document.querySelector("#popupEric")
 
-const popupEric = document.querySelector("#popupBakeryEric");
+const popupEric = document.querySelector("#popupBakeryEric")
 
-const closeButEric = document.querySelector("#close-btnEric");
+const closeButEric = document.querySelector("#close-btnEric")
 
 clickEric.addEventListener("click", function () {
-  popupEric.style.display = "flex";
-  main.classList.add("activePopup");
-  body.classList.add("bodyActivePopup");
-});
+  popupEric.style.display = "flex"
+  main.classList.add("activePopup")
+  body.classList.add("bodyActivePopup")
+})
 
 closeButEric.addEventListener("click", function () {
-  popupEric.style.display = "none";
-  main.classList.remove("activePopup");
-  body.classList.remove("bodyActivePopup");
-});
+  popupEric.style.display = "none"
+  main.classList.remove("activePopup")
+  body.classList.remove("bodyActivePopup")
+})
 
 ///////////////////// Click pop up ////////////////////////////////////////////////
--- appTop3.js
+++ appTop3.js
@@ -1,15 +1,15 @@
 ///////////////////// NAVBAR JS ///////////////////////////////////////
 
-const sidenav = document.getElementById("mySidenav");
-const openBtn = document.getElementById("openBtn");
-openBtn.onclick = closeOrOpenNav;
+const sidenav = document.getElementById("mySidenav")
+const openBtn = document.getElementById("openBtn")
+openBtn.onclick = closeOrOpenNav
 function closeOrOpenNav() {
   if (sidenav.classList.contains("active")) {
-    sidenav.classList.remove("active");
-    sidenav.classList.add("transitionOut");
+    sidenav.classList.remove("active")
+    sidenav.classList.add("transitionOut")
   } else {
-    sidenav.classList.add("active");
-    sidenav.classList.remove("transitionOut");
+    sidenav.classList.add("active")
+    sidenav.classList.remove("transitionOut")
   }
 }
 ///////////////////// NAVBAR JS ///////////////////////////////////////
@@ -17,56 +17,56 @@ function closeOrOpenNav() {
 ///////////////////// Click pop up ////////////////////////////////////////////////
 
 //when the user click on Auguste picture
-const clickAuguste = document.querySelector("#popupAuguste");
+const clickAuguste = document.querySelector("#popupAuguste")
 
-const popupAuguste = document.querySelector("#popupBakeryAuguste");
+const popupAuguste = document.querySelector("#popupBakeryAuguste")
 
-const main = document.querySelector(".main");
+const main = document.querySelector(".main")
 
-const closeButAuguste = document.querySelector("#close-btnAuguste");
+const closeButAuguste = document.querySelector("#close-btnAuguste")
 
 clickAuguste.addEventListener("click", function () {
-  popupAuguste.style.display = "flex";
-  main.classList.add("bodyActivePopup");
-});
+  popupAuguste.style.display = "flex"
+  main.classList.add("bodyActivePopup")
+})
 
 closeButAuguste.addEventListener("click", function () {
-  popupAuguste.style.display = "none";
-  main.classList.remove("bodyActivePopup");
-});
+  popupAuguste.style.display = "none"
+  main.classList.remove("bodyActivePopup")
+})
 
 //when the user click on Toque picture
-const clickToque = document.querySelector("#popupToque");
+const clickToque = document.querySelector("#popupToque")
 
-const popupToque = document.querySelector("#popupBakeryToque");
+const popupToque = document.querySelector("#popupBakeryToque")
 
-const closeButToque = document.querySelector("#close-btnToque");
+const closeButToque = document.querySelector("#close-btnToque")
 
 clickToque.addEventListener("click", function () {
-  popupToque.style.display = "flex";
-  main.classList.add("bodyActivePopup");
-});
+  popupToque.style.display = "flex"
+  main.classList.add("bodyActivePopup")
+})
 
 closeButToque.addEventListener("click", function () {
-  popupToque.style.display = "none";
-  main.classList.remove("bodyActivePopup");
-});
+  popupToque.style.display = "none"
+  main.classList.remove("bodyActivePopup")
+})
 
 //when the user click on Eric picture
-const clickEric = document.querySelector("#popupEric");
+const clickEric = document.querySelector("#popupEric")
 
-const popupEric = document.querySelector("#popupBakeryEric");
+const popupEric = document.querySelector("#popupBakeryEric")
 
-const closeButEric = document.querySelector("#close-btnEric");
+const closeButEric = document.querySelector("#close-btnEric")
 
 clickEric.addEventListener("click", function () {
-  popupEric.style.display = "flex";
-  main.classList.add("bodyActivePopup");
-});
+  popupEric.style.display = "flex"
+  main.classList.add("bodyActivePopup")
+})
 
 closeButEric.addEventListener("click", function () {
-  popupEric.style.display = "none";
-  main.classList.remove("bodyActivePopup");
-});
+  popupEric.style.display = "none"
+  main.classList.remove("bodyActivePopup")
+})
 
 ///////////////////// Click pop up ////////////////////////////////////////////////
